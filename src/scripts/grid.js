dbe0b698b0c2e5847e77a8bfcbe83da5ddf46346
Evan Li <evanli@Evans-MBP.cable.rcn.com>
2020-11-30 19:00:03 -0500

    Added dropdown button above grid

--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -9,6 +9,54 @@ const grid = new Grid({
     data: gridData.schoolData
 });
 
+//code for dropdown buttom
+function toggleClass(elem,className){
+    if (elem.className.indexOf(className) !== -1){
+      elem.className = elem.className.replace(className,'');
+    }
+    else{
+      elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
+    }
+  
+    return elem;
+}
+  
+function toggleMenuDisplay(e){
+    const dropdown = e.currentTarget.parentNode;
+    const menu = dropdown.querySelector('.menu');
+    const icon = dropdown.querySelector('.fa-angle-right');
+  
+    toggleClass(menu,'hide');
+    toggleClass(icon,'rotate-90');
+}
+  
+function handleOptionSelected(e){	
+  
+    const id = e.target.id;
+    const newValue = 'Added ' + e.target.textContent + ' ';
+    const titleElem = document.querySelector('.dropdown .title');
+    const icon = document.querySelector('.dropdown .title .fa');
+      
+    var element = document.getElementById(id);
+    element.style.display = "none"
+  
+    titleElem.textContent = newValue;
+    titleElem.appendChild(icon);
+  
+    //trigger custom event
+    document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
+      //setTimeout is used so transition is properly shown
+    setTimeout(() => toggleClass(icon,'rotate-90',0));
+}
+
 export default function () {
     grid.render(document.getElementById('grid'));
+    
+    //get elements
+    const dropdownTitle = document.querySelector('.dropdown .title');
+    const dropdownOptions = document.querySelectorAll('.dropdown .option');
+
+    //bind listeners to these elements
+    dropdownTitle.addEventListener('click', toggleMenuDisplay);
+    dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
 }
